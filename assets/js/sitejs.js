29fd373fd3841e0598c3fdfce3ce3072f3f2a429
unknown <west4@student.op.ac.nz>
2020-06-04 21:31:04 +1200

    Added old page

---
+++ assets/js/sitejs.js
@@ -0,0 +1,64 @@
+/*(function(){
+var ul = document.createElement('ul');
+    ul.setAttribute('id','top10');
+        
+    document.getElementById('renderList').appendChild(ul);
+
+ 
+fetch("../assets/js/artists.json")
+ 
+
+    .then(artistsResponse => {
+        return artistsResponse.json();
+    })
+
+    .then (artistData => {
+        console.log(artistData["Artists"])
+        artistData["Artists"].forEach(Artist => {
+            
+        });
+    })
+
+        
+fetch("../assets/js/songs.json")
+    
+    .then(songsResponse =>{
+        return songsResponse.json();
+    })
+    
+    .then(songdata => {
+        console.log(songdata["Songs"])
+    })
+})
+   
+
+    /*data ["img"][0].forEach(Img =>{
+
+    });
+
+    for (let i = 0; i < "Data"[0]["Artists"]; i++) {
+        Artists[i].src = (data["Data"][0]["Artist"][i]["img"][i]);
+    
+    }
+    
+    for (let i = 0; i < "Data"[0]["Songs"]; i++) {
+        Songs[i].src = (data["Data"][0]["Artist"][i]["Song"][i]["Scrobbles"][i]);
+    
+    
+    }
+    for (let i = 0; i < data.length; i++) {
+        musicdata = data[i];
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(musicdata.))
+    }
+
+/*for (let i = 0; i < "Data"[0]["Artists"]; i++) {
+    Artists[i].src = (data["Data"][0]["Artist"][i]["img"][i]);
+
+}
+
+for (let i = 0; i < "Data"[0]["Songs"]; i++) {
+    Songs[i].src = (data["Data"][0]["Artist"][i]["Song"][i]["Scrobbles"]);
+
+
+}*/
